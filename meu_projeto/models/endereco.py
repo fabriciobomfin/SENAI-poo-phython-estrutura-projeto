5c8ecc61e6b19e24dc07d46fd852ead5033d68cc
Fabrício <134095128+fabriciobomfin@users.noreply.github.com>
2024-09-11 11:19:10 +0000

    update

--- meu_projeto/models/endereco.py
+++ meu_projeto/models/endereco.py
@@ -1,10 +1,10 @@
 class Endereco:
-    def __init__(self, logradouro: str, numero: int) -> None:
-        self.logradouro = logradouro
+    def __init__(self, local: str, numero: int) -> None:
+        self.local = local
         self.numero = numero
 
     def __str__(self) -> str:
         return (
-            f"\nLogradouro: {self.logradouro}"
+            f"\nLocal: {self.local}"  # Corrigido a capitalização de "local"
             f"\nNúmero: {self.numero}"
         )
--- meu_projeto/models/pessoa.py
+++ meu_projeto/models/pessoa.py
@@ -1,17 +1,17 @@
-
 from models.enums.sexo import Sexo
 from models.endereco import Endereco
 
 class Pessoa:
     def __init__(self, nome: str, idade: int, sexo: Sexo, endereco: Endereco) -> None:
-        
         self.nome = nome
-        self.idade = idade 
-        self.sexo = sexo 
+        self.idade = idade
+        self.sexo = sexo
         self.endereco = endereco
 
     def __str__(self) -> str:
-        return(f"\nNome: {self.nome}" 
-               f"\nIdade: {self.idade}",
-               f"\nEndereço: {self.endereco}"
-               f"\nSexo: {self.sexo.value}")
+        return (
+            f"\nNome: {self.nome}"
+            f"\nIdade: {self.idade}"
+            f"\nEndereço: {self.endereco}"
+            f"\nSexo: {self.sexo.value}"
+        )
